305bebf443d26f02b2738c3969d29b4b9c858f3c
Darius Robinson <robinson.darius090@gmail.com>
2022-06-22 16:24:08 -0400

    added event listeners for game board to place x and o

--- app.js
+++ app.js
@@ -13,11 +13,16 @@ function renderState() {
 // maybe a dozen or so helper functions for tiny pieces of the interface
 
 // listeners
-function onBoardClick() {
-  // update state, maybe with another dozen or so helper functions...
+
+// update state, maybe with another dozen or so helper functions..
+function onBoardClick(event) {
+    event.target.innerText = 'O'
+    event.target.innerText = 'x'
+  
 
   renderState() // show the user the new state
 }
-const gameBoard = document.getElementById('gameBoard');
 
-gameBoard.addEventListener('click', onBoardClick);
+const board = document.getElementById('gameBoard');
+console.log(board)
+board.addEventListener('click', onBoardClick);
