ed259e529f2f810d3af56d4ca28f0da6a14ca938
Darius Robinson <robinson.darius090@gmail.com>
2022-06-24 01:09:27 -0400

    established board as a variable

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 // define the starting state of the game 
 const initialState = {
-  player1 = "X"
-  player2 = "O"
-  board: [
+ const player1 = "X";
+ const  player2 = "O";
+  let board = [
     [null, null, null],
     [null, null, null],
     [null, null, null]
-  ]
+  ];
 // winning combos to be checked ever time 
   winCombos = [
   [0-0, 0-1, 0-2],
@@ -17,10 +17,12 @@ const initialState = {
   [0-2, 1-2, 2-2],
   [0-0, 1-1, 2-2],
   [0-2, 1-1, 2-0]
-  ]
+ ]
 }
 
 function buildInitialState() {
+  
+
 
 }
 
@@ -35,9 +37,9 @@ function renderState() {
 
 // update state, maybe with another dozen or so helper functions..
 function onBoardClick(event) {
-    event.target.innerText = 'O'
+    event.target.innerText = 'O';
      
-    event.target.innerText = 'x'
+    event.target.innerText = 'x';
   
 
   renderState() // show the user the new state
