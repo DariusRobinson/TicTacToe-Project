cb2125e87472f33b5d0f015c0f5a6b9689c61316
Darius Robinson <robinson.darius090@gmail.com>
2022-06-24 16:36:41 -0400

    working click for board and counter to establish turns...

--- app.js
+++ app.js
@@ -1,61 +1,60 @@
-// define the starting state of the game 
+// define the starting state of the game using initialState as an Object and they keys being
+// player1, player2, board, and count... and corresponding value pairs
+//"X"         "O"    [null]      counts even and odd to calculate turns
 const initialState = {
- const player1 = "X";
- const  player2 = "O";
-  let board = [
+  player1: "X",
+  player2: "O",
+  board: [
     [null, null, null],
     [null, null, null],
-    [null, null, null]
-  ];
-// winning combos to be checked ever time 
-  winCombos = [
-  [0-0, 0-1, 0-2],
-  [1-0, 1-1, 1-2],
-  [2-0, 2-1, 2-2],
-  [0-0, 1-0, 2-0],
-  [0-1, 1-1, 2-1],
-  [0-2, 1-2, 2-2],
-  [0-0, 1-1, 2-2],
-  [0-2, 1-1, 2-0]
- ]
- let cellElement = document.querySelectorAll('td')  
+    [null, null, null],
+  ],
+  count: 0,
+};
+// winning combos to be checked ever time
+winCombos = [
+  [0 - 0, 0 - 1, 0 - 2],
+  [1 - 0, 1 - 1, 1 - 2],
+  [2 - 0, 2 - 1, 2 - 2],
+  [0 - 0, 1 - 0, 2 - 0],
+  [0 - 1, 1 - 1, 2 - 1],
+  [0 - 2, 1 - 2, 2 - 2],
+  [0 - 0, 1 - 1, 2 - 2],
+  [0 - 2, 1 - 1, 2 - 0],
+];
+let cellElement = document.querySelectorAll("td");
 //^^pulling all the table cell elements
 
- let board = document.getElementById('board')
+let board = document.getElementById("board");
 
- let resetButton = document.getElementById('resetButton')
+let resetButton = document.getElementById("resetButton");
 
- let winMessage = document.getElementById('you-Win')
-}
+let winMessage = document.getElementById("you-Win");
+
 // function to begin game and enter names etc...
-function buildInitialState() {
-  
+function buildInitialState() {}
 
 
-}
 
 // render
-function renderState() {
-
-}
+function renderState() {}
 
 // maybe a dozen or so helper functions for tiny pieces of the interface
 
 // listeners
 
-// update state, maybe with another dozen or so helper functions..
-function onBoardClick(event) {
-  
-  document.addEventListener('click', (event) => )
-  event.target.innerText = 'O';
-     
-    // event.target.innerText = 'x';
-  
+board.addEventListener("click", (event) => {
+  if (event.target.innerText === "") {
+    if (initialState.count % 2 === 0) {
+      event.target.innerText = "x";
+      initialState.count++;
+    } else {
+      event.target.innerText = "o";
+      initialState.count++;
+    }
+  }
+});
 
-  renderState() // show the user the new state
-}
-
-const board = document.getElementById('gameBoard');
-console.log(board)
-board.addEventListener('click', onBoardClick);
-
+// const board = document.getElementById('gameBoard');
+// console.log(board)
+// board.addEventListener('click', onBoardClick);
