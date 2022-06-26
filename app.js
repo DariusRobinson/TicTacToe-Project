25fc0150f7b27607fdf8e819ab2ae45b3cf0894f
Darius Robinson <robinson.darius090@gmail.com>
2022-06-26 04:02:38 -0400

    function begining

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 // player1, player2, board, and count... and corresponding value pairs
 //"X"         "O"    [null]      counts even and odd to calculate turns
 const initialState = {
-  player1: "X",
-  player2: "O",
+  // player1: "X",
+  // player2: "O",
   board: [
     [null, null, null],
     [null, null, null],
@@ -12,7 +12,7 @@ const initialState = {
   count: 0,
 };
 // winning combos to be checked ever time
-winCombos = [
+const winCombos = [
   [0 - 0, 0 - 1, 0 - 2],
   [1 - 0, 1 - 1, 1 - 2],
   [2 - 0, 2 - 1, 2 - 2],
@@ -23,53 +23,93 @@ winCombos = [
   [0 - 2, 1 - 1, 2 - 0],
 ];
 
+const cells = document.querySelectorAll(".cell");
+//Players and turn settings
+const Player1 ="X";
+const Player2 ="O";
+let turn = Player1;
+
+
+const boardState = Array(cells.length);
+boardState.fill(null); //automatically fill all tiles with null value
+console.log(boardState);
+
+
 //Elements
-const cellElement = document.querySelectorAll("td");
+
 const board = document.getElementById("board");
 const resetButton = document.getElementById("resetButton");
-console.log(board)
-const square1 = document.getElementById("0-0")
-const square2 = document.getElementById("0-1")
-const square3 = document.getElementById("0-2")
+console.log(board);
+const square1 = document.getElementById("0-0");
+const square2 = document.getElementById("0-1");
+const square3 = document.getElementById("0-2");
 //^^Row 1
-const square4 = document.getElementById("1-0")
-const square5 = document.getElementById("1-1")
-const square6 = document.getElementById("1-2")
+const square4 = document.getElementById("1-0");
+const square5 = document.getElementById("1-1");
+const square6 = document.getElementById("1-2");
 //^^ Row 2
-const square7 = document.getElementById("2-0")
-const square8 = document.getElementById("2-1")
-const square9 = document.getElementById("2-2")
-
-
-
+const square7 = document.getElementById("2-0");
+const square8 = document.getElementById("2-1");
+const square9 = document.getElementById("2-2");
+//^^Row 3
 const winMessage = document.getElementById("you-Win");
+const playAgainBox = document.getElementById("Game-over-box");
+const winnerIsText = document.getElementById("WinnerIsText")
 
-board.addEventListener("click", (event) => {
-const cor = event.target.id
+//Event listener for game board click
 
 
-// console.log(cor)
-const row = cor[0]
-const column = cor[2]
-initialState.board[row][column]= ''
-// console.log(initialState.board)
-// // console.log(cor[0])
-// console.log(cor[2])
 
-  if (event.target.innerText === "") {
-    if (initialState.count % 2 === 0) {
-      event.target.innerText = "x";
-      initialState.count++;
-    } else {
-      event.target.innerText = "o";
-      initialState.count++;
-    }
+cells.forEach((Cell) => Cell.addEventListener("click", cellClick));
+
+function cellClick(event){
+  if (playAgainBox.classList.contains("visible")){
+    return;
   }
-});
+
+  const Cell = event.target;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Stevens code he showed you in the last office hour...
+
+// board.addEventListener("click", (event) => {
+//   const cor = event.target.id;
+
+//   // console.log(cor)
+//   const row = cor[0];
+//   const column = cor[2];
+//   initialState.board[row][column] = "";
+//   // console.log(initialState.board)
+//   // // console.log(cor[0])
+//   // console.log(cor[2])
+
+//   if (event.target.innerText === "") {
+//     if (initialState.count % 2 === 0) {
+//       event.target.innerText = "x";
+//       initialState.count++;
+//     } else {
+//       event.target.innerText = "o";
+//       initialState.count++;
+//     }
+//   }
+// });
 
 // const board = document.getElementById('gameBoard');
 // console.log(board)
 // board.addEventListener('click', onBoardClick);
-
-
-
